508186314f39552c865aa828f1d4522e42e72484
YvCeung <zytuatua@163.com>
2025-04-29 16:09:29 +0800

    feature:http中支持HttpPool中对哈希一致性算法以及node跟PeerGetter之间的映射

--- pkg/remoting/rest/http.go
+++ pkg/remoting/rest/http.go
@@ -4,21 +4,38 @@ package rest
 import (
 	"fmt"
 	"github.com/YvCeung/easy-cache/pkg/cache"
+	"github.com/YvCeung/easy-cache/pkg/consistenthash"
+	"github.com/YvCeung/easy-cache/pkg/multinode"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 )
 
 // 这个相当于Java中的习惯，会给每一个请求路径统一都加个前缀
-const defaultBase = "/easycache/"
+const (
+	defaultBase = "/easycache/"
+	//默认50个虚拟节点
+	defaultReplicas = 50
+)
 
 type HttpPool struct {
 	//标志当前节点的地址 协议+ip+端口
 	selfNode string
 	//请求uri,里面包含一些业务参数
 	baseBizPath string
+
+	//下面的这几个属性都是为了节点选择的功能而做
+
+	//锁
+	mu sync.Mutex
+
+	//一致性hash算法中的容器
+	peers *consistenthash.ConsistentHash
+	//真实节点的uri跟对应的*httpGetter之间的映射关系
+	httpGetters map[string]*httpGetter
 }
 
 func NewHttpPool(selfNode string) *HttpPool {
@@ -91,4 +108,34 @@ func (h *httpGetter) Get(group string, key string) ([]byte, error) {
 		return nil, fmt.Errorf("read responsebody error: %v", err)
 	}
 	return data, nil
+}
+
+// 可以理解为分布式节点dispatcher
+func (httpPool *HttpPool) PickPeer(key string) (multinode.PeerGetter, bool) {
+	httpPool.mu.Lock()
+	defer httpPool.mu.Unlock()
+
+	if node := httpPool.peers.GetNode(key); node != "" && node != httpPool.selfNode {
+		httpPool.Log("Pick peer(node) %s", node)
+		return httpPool.httpGetters[node], true
+	}
+	return nil, false
+}
+
+//还需要提供一个初始化HttpPool里面一致性哈希算法以及node跟Getter之间映射关系等数据的方法
+func (httpPool *HttpPool) Set(nodes ...string) {
+	httpPool.mu.Lock()
+	defer httpPool.mu.Unlock()
+	//初始化一致性哈希的容器
+	httpPool.peers = consistenthash.New(defaultReplicas, nil)
+	httpPool.peers.Add(nodes...)
+
+	//构建getter的映射关系
+	httpPool.httpGetters = make(map[string]*httpGetter, len(nodes))
+	for _, node := range nodes {
+		httpPool.httpGetters[node] = &httpGetter{
+			//http:xxxx//easycache/
+			baseURL: node + httpPool.baseBizPath,
+		}
+	}
 }
